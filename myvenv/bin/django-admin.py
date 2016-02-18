836d13bbb833befdc109a8f3c0c40a132fa50f1c
imann24 <imann24@gmail.com>
2016-02-18 10:27:30 -0500

    Restarting tutorial with python3

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/imann24/Documents/Current_Projects/hello_world-django/myvenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
