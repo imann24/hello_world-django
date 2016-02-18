a2f05d6b9a21d701f5837922b177fe49b4a13062
imann24 <imann24@gmail.com>
2016-02-18 10:28:17 -0500

    removing virtual env from commit

--- myvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/imann24/Documents/Current_Projects/hello_world-django/myvenv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
